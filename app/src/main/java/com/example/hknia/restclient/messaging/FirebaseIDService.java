16044c7d13b11863e163c691c0b88a1616d424b8
hknia <hamza.khan.niazi04@gmail.com>
2018-02-01 10:43:33 +0500

    Push notification done

---
+++ app/src/main/java/com/example/hknia/restclient/messaging/FirebaseIDService.java
@@ -0,0 +1,52 @@
+package com.example.hknia.restclient.messaging;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+
+import com.example.hknia.restclient.core.Config;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by hknia on 1/26/2018.
+ */
+
+public class FirebaseIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = FirebaseIDService.class.getSimpleName();
+
+
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        // Saving reg id to shared preferences
+        storeRegIdInPref(refreshedToken);
+
+        // sending reg id to your server
+        sendRegistrationToServer(refreshedToken);
+
+        // Notify UI that registration has completed, so the progress indicator can be hidden.
+        Intent registrationComplete = new Intent(Config.REGISTRATION_COMPLETE);
+        registrationComplete.putExtra("token", refreshedToken);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(registrationComplete);
+    }
+
+    private void sendRegistrationToServer(final String token) {
+        // sending gcm token to server
+        Log.e(TAG, "sendRegistrationToServer: " + token);
+    }
+
+    private void storeRegIdInPref(String token) {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences(Config.SHARED_PREF, 0);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("regId", token);
+        editor.commit();
+    }
+}
